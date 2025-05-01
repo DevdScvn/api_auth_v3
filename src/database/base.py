fe4ae6d3644e9abadf2e2903e0af9d5daa39fbbd
DevdScvn <nikita@MacBook-Air-Nikita.local>
2025-05-01 21:41:14 +0300

    commit

---
+++ src/database/base.py
@@ -0,0 +1,35 @@
+import datetime
+
+from sqlalchemy import MetaData, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+from settings.config import settings
+from utils.case_converter import camel_case_to_snake_case
+
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+
+class TimeStampModel(Base):
+
+    __abstract__ = True
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), index=True
+    )
+    deleted_at: Mapped[datetime.datetime | None] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
