a5658843cee611b87db492f01d36458e2fdc3b5c
DevdScvn <nikita@MacBook-Air-Nikita.local>
2025-05-02 02:34:09 +0300

    add admin panel

--- src/main.py
+++ src/main.py
@@ -1,21 +1,26 @@
+import logging
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from sqladmin import Admin
 
+from admin_panel.auth import authentication_backend
+from admin_panel.user import UserAdmin
 from database import db_helper
 from settings.config import settings
 
 from users.router import router as users_router
 from auth.router import router as auth_router
 
+log = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     #startup
     yield
     #shutdown
-    print("dispose_engine")
+    log.warning("dispose_engine")
     await db_helper.dispose()
 
 main_app = FastAPI(
@@ -25,6 +30,11 @@ main_app = FastAPI(
 main_app.include_router(users_router)
 main_app.include_router(auth_router)
 
+admin = Admin(
+    main_app, engine=db_helper.engine, authentication_backend=authentication_backend
+)
+
+admin.add_view(UserAdmin)
 
 if __name__ == "__main__":
     uvicorn.run("main:main_app",
