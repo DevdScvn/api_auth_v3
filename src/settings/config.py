0026adb21185c430f8f75d35126a14824ff318ea
DevdScvn <nikita@MacBook-Air-Nikita.local>
2025-05-01 21:27:52 +0300

    base config

---
+++ src/settings/config.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 8000
